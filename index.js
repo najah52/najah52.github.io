0b24ebfdf308ff6beb22e1866810f3d225c14356
najah52 <35869254+najah52@users.noreply.github.com>
2022-05-05 17:18:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,573 @@
+var vocabulaire = [
+
+  // Demon Slayer - Opening 1
+  {
+    mot : "強",
+    transcription : "強い",
+    furigana : "つよい",
+    traduction : "fort, puissant ; résistant, durable"
+  },
+  {
+    mot : "理由",
+    transcription : "理由",
+    furigana : "りゆう",
+    traduction : "raison"
+  },
+  {
+    mot : "知",
+    transcription : "知る",
+    furigana : "しる",
+    traduction : "savoir, connaître"
+  },
+  {
+    mot : "僕",
+    transcription : "僕",
+    furigana : "ぼく",
+    traduction : "moi, je"
+  },
+  {
+    mot : "連",
+    transcription : "連れる",
+    furigana : "つれる",
+    traduction : "suivre, accompagner"
+  },
+  {
+    mot : "進",
+    transcription : "進める",
+    furigana : "すすめる",
+    traduction : "faire progresser, promouvoir; se dépêcher"
+  },
+  {
+    mot : "泥",
+    transcription : "泥",
+    furigana : "どろ",
+    traduction : "boue"
+  },
+  {
+    mot : "走馬灯",
+    transcription : "走馬灯",
+    furigana : "ぞうまとう",
+    traduction : "le fait de voir sa vie défiler devant soi lorsque l'on est sur le point de mourir"
+  },
+  {
+    mot : "酔",
+    transcription : "酔う",
+    furigana : "よう",
+    traduction : "s'enivrer, se saouler, être intoxiqué, avoir le mal de mer"
+  },
+  {
+    mot : "心",
+    transcription : "心",
+    furigana : "こころ",
+    traduction : "coeur, esprit, âme"
+  },
+  {
+    mot : "震",
+    transcription : "震える",
+    furigana : "ふるえる",
+    traduction : "trembler, frémir, tressaillir, frissonner, palpiter"
+  },
+  {
+    mot : "手",
+    transcription : "手",
+    furigana : "て",
+    traduction : "main; habileté"
+  },
+  {
+    mot : "掴",
+    transcription : "掴む",
+    furigana : "つかむ",
+    traduction : "attraper, empoigner, mettre la main sur, saisir; comprendre, appréhender"
+  },
+  {
+    mot : "夜",
+    transcription : "夜",
+    furigana : "よる",
+    traduction : "nuit"
+  },
+  {
+    mot : "匂",
+    transcription : "匂い",
+    furigana : "におい",
+    traduction : "odeur"
+  },
+  {
+    mot : "空",
+    transcription : "空",
+    furigana : "そら",
+    traduction : "ciel; paradis"
+  },
+  {
+    mot : "睨",
+    transcription : "睨む",
+    furigana : "にらむ",
+    traduction : "fixer du regard; examiner, estimer, garder un oeil sur quelqu'un"
+  },
+  {
+    mot : "変",
+    transcription : "変わる",
+    furigana : "かわる",
+    traduction : "changer, être changé; aller à, être inhabituel, être différent"
+  },
+  {
+    mot : "自分",
+    transcription : "自分",
+    furigana : "じぶん",
+    traduction : "soi, soi-même"
+  },
+  {
+    mot : "自身",
+    transcription : "自身",
+    furigana : "じしん",
+    traduction : "soi-même"
+  },
+  {
+    mot : "消",
+    transcription : "消す",
+    furigana : "けす",
+    traduction : "effacer, gommer; éteindre"
+  },
+  {
+    mot : "夢",
+    transcription : "夢",
+    furigana : "ゆめ",
+    traduction : "rêve"
+  },
+  {
+    mot : "止",
+    transcription : "止まる",
+    furigana : "とまる",
+    traduction : "s'arrêter, faire une pause; se percher, se jucher, se poser"
+  },
+  {
+    mot : "今",
+    transcription : "今",
+    furigana : "いま",
+    traduction : "maintenant, en ce moment, immédiatement"
+  },
+  {
+    mot : "誰",
+    transcription : "誰",
+    furigana : "だれ",
+    traduction : "qui"
+  },
+  {
+    mot : "誰",
+    transcription : "誰か",
+    furigana : "だれか",
+    traduction : "quelqu'un"
+  },
+  {
+    mot : "何度",
+    transcription : "何度",
+    furigana : "なんど",
+    traduction : "nombre de fois"
+  },
+  {
+    mot : "立",
+    transcription : "立ち上がる",
+    furigana : "たちあがる",
+    traduction : "se lever, se mettre debout; commencer, débuter"
+  },
+  {
+    mot : "上",
+    transcription : "立ち上がる",
+    furigana : "たちあがれる",
+    traduction : "se lever, se mettre debout; commencer, débuter"
+  },
+  {
+    mot : "世界",
+    transcription : "世界",
+    furigana : "せかい",
+    traduction : "monde, univers"
+  },
+  {
+    mot : "打",
+    transcription : "打ちのめす",
+    furigana : "うちのめす",
+    traduction : "rouer de coups, passer à tabac, tabasser"
+  },
+  {
+    mot : "負",
+    transcription : "負ける",
+    furigana : "まける",
+    traduction : "perdre, être vaincu, être battu, succomber, être inférieur; baisser le prix"
+  },
+  {
+    mot : "意味",
+    transcription : "意味",
+    furigana : "いみ",
+    traduction : "signification, sens"
+  },
+  {
+    mot : "紅蓮",
+    transcription : "紅蓮",
+    furigana : "ぐれん",
+    traduction : "Gurenge"
+  },
+  {
+    mot : "華",
+    transcription : "華",
+    furigana : "はな",
+    traduction : "fleur, beauté, brillance"
+  },
+  {
+    mot : "咲",
+    transcription : "咲き誇る",
+    furigana : "さきほこる",
+    traduction : "être en pleine floraison"
+  },
+  {
+    mot : "誇",
+    transcription : "咲き誇る",
+    furigana : "さきほこる",
+    traduction : "être en pleine floraison"
+  },
+  {
+    mot : "運命",
+    transcription : "運命",
+    furigana : "うんめい",
+    traduction : "sort, destin"
+  },
+  {
+    mot : "照",
+    transcription : "照らす",
+    furigana : "てらす",
+    traduction : "éclairer, illuminer"
+  },
+  // L'attaque des titans
+  {
+    mot : "美",
+    transcription : "美",
+    furigana : "び",
+    traduction : "beau, beauté"
+  },
+  {
+    mot : "残酷",
+    transcription : "残酷",
+    furigana : "ざんこく",
+    traduction : "cruel"
+  },
+  {
+    mot : "居場所",
+    transcription : "居場所",
+    furigana : "いばしょ",
+    traduction : "emplacement, localisation"
+  },
+  {
+    mot : "命",
+    transcription : "命",
+    furigana : "いのち",
+    traduction : "vie ; chose la plus importante"
+  },
+  {
+    mot : "壊",
+    transcription : "壊れ物",
+    furigana : "こわれもの",
+    traduction : "objet fragile"
+  },
+  {
+    mot : "捨",
+    transcription : "捨てる",
+    furigana : "すてる",
+    traduction : "abandonner (quelqu'un), jeter, rejeter"
+  },
+  {
+    mot : "見",
+    transcription : "見つける",
+    furigana : "みつける",
+    traduction : "trouver, retrouver, découvrir, repérer, localiser ; être habitué à (voir)"
+  },
+  {
+    mot : "安",
+    transcription : "安らか",
+    furigana : "やすらか",
+    traduction : "calme, tranquille, reposant, paisible"
+  },
+  {
+    mot : "眠",
+    transcription : "眠むる",
+    furigana : "ねむる",
+    traduction : "sommeiller, dormir ; mourir ; fermer les yeux"
+  },
+  {
+    mot : "脈",
+    transcription : "脈",
+    furigana : "みゃく",
+    traduction : "pouls"
+  },
+  {
+    mot : "打",
+    transcription : "打つ",
+    furigana : "うつ",
+    traduction : "frapper (quelque chose d'inanimé), taper (sur quelque chose), tapoter (sur une touche) ; injecter ; visiter (pèlerinage) ; jouer ; lancer"
+  },
+  {
+    mot : "衝動",
+    transcription : "衝動",
+    furigana : "しょうどう",
+    traduction : "forte envie, impulsion, désir"
+  },
+  {
+    mot : "願",
+    transcription : "願い",
+    furigana : "ねがい",
+    traduction : "souhait, désir, requête"
+  },
+  {
+    mot : "犯",
+    transcription : "犯す",
+    furigana : "おかす",
+    traduction : "commettre, violer, envahir ; blesser, faire du tort"
+  },
+  {
+    mot : "忘",
+    transcription : "忘れる",
+    furigana : "わすれる",
+    traduction : "oublier"
+  },
+  {
+    mot : "想",
+    transcription : "想う",
+    furigana : "おもう",
+    traduction : "penser, croire, imaginer, sentir ; espérer ; se rappeler"
+  },
+  {
+    mot : "出",
+    transcription : "出す",
+    furigana : "だす",
+    traduction : "faire sortir, envoyer ; produire, présenter"
+  },
+  {
+    mot : "想",
+    transcription : "想い出す",
+    furigana : "おもいだす",
+    traduction : "se souvenir, se rappeler de"
+  },
+  {
+    mot : "出",
+    transcription : "想い出す",
+    furigana : "おもいだす",
+    traduction : "se souvenir, se rappeler de"
+  },
+  {
+    mot : "生",
+    transcription : "生きる",
+    furigana : "いきる",
+    traduction : "vivre"
+  },
+  {
+    mot : "何故",
+    transcription : "何故",
+    furigana : "なぜ",
+    traduction : "pourquoi"
+  },
+  {
+    mot : "問",
+    transcription : "問う",
+    furigana : "とう",
+    traduction : "demander, questionner ; charger, accuser ; prendre soin"
+  },
+  {
+    mot : "嗚呼",
+    transcription : "嗚呼",
+    furigana : "ああ",
+    traduction : "ah!, oh!, mon dieu!"
+  },
+  {
+    mot : "弱",
+    transcription : "弱い",
+    furigana : "よわい",
+    traduction : "faible, fragile"
+  },
+  {
+    mot : "弱",
+    transcription : "弱さ",
+    furigana : "よわさ",
+    traduction : "faiblesse"
+  },
+  {
+    mot : "何",
+    transcription : "何",
+    furigana : "なに",
+    traduction : "quoi, que"
+  },
+  {
+    mot : "守",
+    transcription : "守る",
+    furigana : "まもる",
+    traduction : "protéger, garder, respecter ; obéir, observer, suivre, tenir une promesse"
+  },
+  {
+    mot : "理性",
+    transcription : "理性",
+    furigana : "りせい",
+    traduction : "raison, sens"
+  },
+  {
+    mot : "無",
+    transcription : "無い",
+    furigana : "ない",
+    traduction : "verbe exprimant la négation"
+  },
+  // Nekodake no Bakeneko
+  {
+    mot : "岳",
+    transcription : "岳",
+    furigana : "だけ",
+    traduction : "sommet, montagne"
+  },{
+    mot : "化",
+    transcription : "化け猫",
+    furigana : "ばけねこ",
+    traduction : "chat maléfique pouvant se changer en homme"
+  },{
+    mot : "旅",
+    transcription : "旅",
+    furigana : "たび",
+    traduction : "voyage"
+  },{
+    mot : "商人",
+    transcription : "商人",
+    furigana : "しょうにん",
+    traduction : "marchand, commerçant"
+  },{
+    mot : "仕事",
+    transcription : "仕事",
+    furigana : "しごと",
+    traduction : "travail, métier, emploi, tâche"
+  },{
+    mot : "九州",
+    transcription : "九州",
+    furigana : "きゅうしゅう",
+    traduction : "Kyûshû (Une des quatre îles principales du Japon avec Hokkaidô, Honshû et Shikoku)"
+  },{
+    mot : "阿蘇山",
+    transcription : "阿蘇山",
+    furigana : "あそざん",
+    traduction : "mont Aso"
+  },
+  {
+    mot : "奥",
+    transcription : "奥",
+    furigana : "おく",
+    traduction : "fond, intérieur"
+  },
+  {
+    mot : "出",
+    transcription : "出かける",
+    furigana : "でかける",
+    traduction : "sortir, sortir de chez soi"
+  },
+  {
+    mot : "間",
+    transcription : "いつの間に (何時の間に)",
+    furigana : "いつのまに",
+    traduction : "(ne pas savoir) quand"
+  },
+  {
+    mot : "道",
+    transcription : "道",
+    furigana : "みち",
+    traduction : "chemin, voie, route"
+  },
+  {
+    mot : "間違",
+    transcription : "間違える",
+    furigana : "まちがえる",
+    traduction : "se tromper"
+  },
+  {
+    mot : "",
+    transcription : "",
+    furigana : "",
+    traduction : ""
+  },
+  {
+    mot : "",
+    transcription : "",
+    furigana : "",
+    traduction : ""
+  }
+];
+// Fin Demon Slayer - Opening 1
+
+//Remplis la div .info
+var kanjis = document.querySelectorAll("rb");
+kanjis.forEach(function(clic, i){
+  clic.addEventListener("click", function(){
+
+  for (var j = 0; j < vocabulaire.length; j++) {
+
+    if(kanjis[i].innerHTML == vocabulaire[j].mot){
+      document.querySelector(".container").style.display = "block";
+      document.querySelector(".info").innerHTML += "<h3>" + vocabulaire[j].transcription + "</h3><span class='furigana'>" + vocabulaire[j].furigana + "</span><p>" + vocabulaire[j].traduction + "</p><hr>";
+    }
+  }
+
+  })
+})
+
+
+// Fin Remplis div .info
+
+if(document.querySelector(".container")){
+  document.querySelector(".container").addEventListener("click", function(e){
+    if(e.target.classList.contains("info") == false){
+      document.querySelector(".container").style.display = "none";
+      document.querySelector(".info").innerHTML = "";
+    }
+  });
+}
+
+document.querySelector(".menu").style.left = "-100%";
+
+function afficheMenu(){
+  if(document.querySelector(".menu").style.left == "-100%"){
+    document.querySelector(".menu").style.left = 0;
+    document.querySelector(".fermerMenu").style.display = "block";
+  }
+  else {
+    document.querySelector(".menu").style.left = "-100%";
+    document.querySelector(".fermerMenu").style.display = "none";
+  }
+}
+
+/*
+// MENU
+document.querySelectorAll(".listeAnime li a").forEach((item, i) => {
+  item.onclick = function(e){
+    e.preventDefault();
+    document.querySelector("iframe").src = item.href;
+  }
+});
+*/
+
+// Fermer le menu (petit écran)
+document.querySelector(".menu span").onclick = function(){
+  document.querySelector(".menu").style.left = "-100%";
+  document.querySelector(".fermerMenu").style.display = "none";
+}
+
+document.querySelector(".fermerMenu").onclick = function(){
+  document.querySelector(".menu").style.left = "-100%";
+  document.querySelector(".fermerMenu").style.display = "none";
+}
+
+document.querySelectorAll(".menu li").forEach((item, i) => {
+  item.onclick = function(){
+    document.querySelector(".menu").style.left = "-100%";
+    document.querySelector(".fermerMenu").style.display = "none";
+  }
+});
+
+document.querySelectorAll(".subnav h3").forEach((item, i) => {
+  item.onclick = function(){
+    if(document.querySelectorAll(".subnav-content")[i].style.display == "" || document.querySelectorAll(".subnav-content")[i].style.display == "none"){
+      document.querySelectorAll(".subnav-content")[i].style.display = "flex";
+    }
+    else {
+      document.querySelectorAll(".subnav-content")[i].style.display = "none";
+    }
+  }
+});
