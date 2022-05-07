b8c1da7135ac71dbaa8fb4303c82eb995f27a30b
najah52 <35869254+najah52@users.noreply.github.com>
2022-05-07 18:44:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -478,17 +478,522 @@ var vocabulaire = [
     traduction : "se tromper"
   },
   {
-    mot : "",
-    transcription : "",
-    furigana : "",
-    traduction : ""
+    mot : "困",
+    transcription : "困る",
+    furigana : "こまる",
+    traduction : "être embarrassé, se sentir gêné, être ennuyé"
   },
-  {
-    mot : "",
-    transcription : "",
-    furigana : "",
-    traduction : ""
+{
+    mot : "本格的",
+    transcription : "本格的",
+    furigana : "ほんかくてき",
+    traduction : "vrai, véritable, authentique"
+  },
+{
+    mot : "迷",
+    transcription : "迷う",
+    furigana : "まよう",
+    traduction : "se perdre, s'égarer; hésiter, être perplexe; perdre le contrôle de soi, s'abandonner à"
+  },
+{
+    mot : "考",
+    transcription : "考える",
+    furigana : "かんがえる",
+    traduction : "penser, réfléchir"
+  },
+{
+    mot : "泣",
+    transcription : "泣き声",
+    furigana : "なきごえ",
+    traduction : "sanglot, gémissement, pleur"
+  },
+{
+    mot : "声",
+    transcription : "声",
+    furigana : "こえ",
+    traduction : "voix"
+  },
+{
+    mot : "聞",
+    transcription : "聞こえる",
+    furigana : "きこえる",
+    traduction : "être audible, s'entendre, se faire entendre"
+  },
+{
+    mot : "山",
+    transcription : "山",
+    furigana : "やま",
+    traduction : "montagne, colline"
+  },
+{
+    mot : "中",
+    transcription : "中",
+    furigana : "なか",
+    traduction : "milieu, intérieur, parmi; pendant"
+  },
+{
+    mot : "確",
+    transcription : "確か",
+    furigana : "たしか",
+    traduction : "sûr, certain"
+  },
+{
+    mot : "辺",
+    transcription : "辺り",
+    furigana : "あたり",
+    traduction : "aux alentours, au bord, à côté"
+  },
+{
+    mot : "親玉",
+    transcription : "親玉",
+    furigana : "おやだま",
+    traduction : "boss, chef, leader d'un groupe (à connotation péjorative, à mauvais desseins)"
+  },
+{
+    mot : "聞",
+    transcription : "聞く",
+    furigana : "きく",
+    traduction : "entendre, écouter passivement"
+  },
+{
+    mot : "事",
+    transcription : "事",
+    furigana : "こと",
+    traduction : "chose, affaire, fait; incident; situation"
+  },
+{
+    mot : "捕",
+    transcription : "捕まる",
+    furigana : "つかまる",
+    traduction : "se faire prendre, être arrêté, être pris"
+  },
+{
+    mot : "大変",
+    transcription : "大変",
+    furigana : "たいへん",
+    traduction : "important, grave, sérieux"
+  },
+{
+    mot : "場",
+    transcription : "場",
+    furigana : "ば",
+    traduction : "place, endroit, espace, lieu, emplacement"
+  },
+{
+    mot : "離",
+    transcription : "離れる",
+    furigana : "はなれる",
+    traduction : "quitter, s'éloigner, être séparé de"
+  },
+{
+    mot : "方向",
+    transcription : "方向",
+    furigana : "ほうこう",
+    traduction : "direction, orientation, sens"
+  },
+{
+    mot : "先",
+    transcription : "先",
+    furigana : "さき",
+    traduction : "avant, devant, d'abord"
+  },
+{
+    mot : "急",
+    transcription : "急ぐ",
+    furigana : "いそぐ",
+    traduction : "se dépêcher"
+  },
+{
+    mot : "家",
+    transcription : "家",
+    furigana : "いえ",
+    traduction : "maison, foyer; lignée, nom de famille"
+  },
+{
+    mot : "明",
+    transcription : "明かり",
+    furigana : "あかり",
+    traduction : "lumière; lampe"
+  },
+{
+    mot : "物",
+    transcription : "物",
+    furigana : "もの",
+    traduction : "chose"
+  },
+{
+    mot : "見",
+    transcription : "見える",
+    furigana : "みえる",
+    traduction : "se voir, être visible"
+  },
+{
+    mot : "泊",
+    transcription : "泊める",
+    furigana : "とめる",
+    traduction : "héberger, loger"
+  },
+{
+    mot : "近",
+    transcription : "近づく",
+    furigana : "ちかづく",
+    traduction : "se rapprocher"
+  },
+{
+    mot : "近",
+    transcription : "近づける",
+    furigana : "ちかける",
+    traduction : "se rapprocher de, s'approcher de"
+  },
+{
+    mot : "立派",
+    transcription : "立派",
+    furigana : "りっぱ",
+    traduction : "magnifique, majestueux, superbe, louable"
+  },
+{
+    mot : "屋敷",
+    transcription : "屋敷",
+    furigana : "やしき",
+    traduction : "résidence, demeure"
+  },
+{
+    mot : "者",
+    transcription : "者",
+    furigana : "もの",
+    traduction : "personne, homme"
+  },
+{
+    mot : "今夜",
+    transcription : "今夜",
+    furigana : "こんや",
+    traduction : "ce soir, cette nuit"
+  },
+{
+    mot : "女",
+    transcription : "女",
+    furigana : "おんな",
+    traduction : "femme"
+  },
+{
+    mot : "現",
+    transcription : "現れる",
+    furigana : "あらわれる",
+    traduction : "apparaître, émerger, devenir visible"
+  },
+{
+    mot : "座敷",
+    transcription : "座敷",
+    furigana : "ざしき",
+    traduction : "zashiki (salle à tatamis), salon de style japonais"
+  },
+{
+    mot : "通",
+    transcription : "通す",
+    furigana : "とおす",
+    traduction : "faire passer, enfiler; parcourir"
+  },
+{
+    mot : "野宿",
+    transcription : "野宿する",
+    furigana : "のじゅくする",
+    traduction : "dormir à la belle étoile"
+  },
+{
+    mot : "荷物",
+    transcription : "荷物",
+    furigana : "にもつ",
+    traduction : "bagage; fardeau, boulet"
+  },
+{
+    mot : "一息",
+    transcription : "一息",
+    furigana : "ひといき",
+    traduction : "une courte période, une pause"
+  },
+{
+    mot : "言",
+    transcription : "言う",
+    furigana : "いう",
+    traduction : "dire; appeler, nommer"
+  },
+{
+    mot : "風呂",
+    transcription : "風呂",
+    furigana : "ふろ",
+    traduction : "bain"
+  },
+{
+    mot : "廊下",
+    transcription : "廊下",
+    furigana : "ろうか",
+    traduction : "couloir"
+  },
+{
+    mot : "突",
+    transcription : "突きあかり",
+    furigana : "つきあかり",
+    traduction : "fin (d'une rue, d'un couloir)"
+  },
+{
+    mot : "入",
+    transcription : "入る",
+    furigana : "はいる",
+    traduction : "entrer, se joindre à"
+  },
+{
+    mot : "間",
+    transcription : "間",
+    furigana : "あいだ",
+    traduction : "espace, distance, intervalle; entre"
+  },
+{
+    mot : "飯",
+    transcription : "ご飯",
+    furigana : "ごはん",
+    traduction : "riz cuit; repas"
+  },
+{
+    mot : "違",
+    transcription : "違う",
+    furigana : "ちがう",
+    traduction : "être différent, être erroné; ne pas être dans son état normal"
+  },
+{
+    mot : "別",
+    transcription : "別",
+    furigana : "べつ",
+    traduction : "différemment, à part, séparément, de côté"
+  },
+{
+    mot : "驚",
+    transcription : "驚く",
+    furigana : "おどろく",
+    traduction : "s'étonner, être surpris"
+  },
+{
+    mot : "顔",
+    transcription : "顔",
+    furigana : "かお",
+    traduction : "visage"
+  },
+{
+    mot : "耳元",
+    transcription : "耳元",
+    furigana : "みみもと",
+    traduction : "dans le creux de l'oreille, à l'oreille, près de l'oreille"
+  },
+{
+    mot : "口",
+    transcription : "口",
+    furigana : "くち",
+    traduction : "bouche"
+  },
+{
+    mot : "人間",
+    transcription : "人間",
+    furigana : "にんげん",
+    traduction : "être humain, homme, genre humain, personne; caractère d'une personne"
+  },
+{
+    mot : "来",
+    transcription : "来る",
+    furigana : "くる",
+    traduction : "venir, commencer"
+  },
+{
+    mot : "逃",
+    transcription : "逃げる",
+    furigana : "にげる",
+    traduction : "s'échapper, fuir, s'échapper"
+  },
+{
+    mot : "姿",
+    transcription : "姿",
+    furigana : "すがた",
+    traduction : "silhouette"
+  },
+{
+    mot : "三毛",
+    transcription : "三毛猫",
+    furigana : "みけねこ",
+    traduction : "chat au pelage tricolore"
+  },
+{
+    mot : "可愛",
+    transcription : "可愛がる",
+    furigana : "かわいがる",
+    traduction : "chérir, gâter; être affectueux"
+  },
+{
+    mot : "年",
+    transcription : "年",
+    furigana : "とし",
+    traduction : "année, âge"
+  },
+{
+    mot : "取",
+    transcription : "取る",
+    furigana : "とる",
+    traduction : "prendre, saisir: voler; enlever"
+  },
+{
+    mot : "仕",
+    transcription : "使える",
+    furigana : "つかえる",
+    traduction : "être utile"
+  },
+{
+    mot : "思",
+    transcription : "思い出す",
+    furigana : "おもいだす",
+    traduction : "se souvenir, se rappeler de"
+  },
+{
+    mot : "前",
+    transcription : "お前",
+    furigana : "おまえ",
+    traduction : "toi, tu"
+  },
+{
+    mot : "急",
+    transcription : "急",
+    furigana : "きゅう",
+    traduction : "soudain; rapide; pressé, urgent; raide"
+  },
+{
+    mot : "心配",
+    transcription : "心配",
+    furigana : "しんぱい",
+    traduction : "inquiétude, souci"
+  },
+{
+    mot : "早",
+    transcription : "早く",
+    furigana : "はやく",
+    traduction : "vite, plus vite, rapidement"
+  },
+{
+    mot : "裏口",
+    transcription : "裏口",
+    furigana : "うらぐち",
+    traduction : "porte de derrière, porte de service"
+  },
+{
+    mot : "教",
+    transcription : "教える",
+    furigana : "おしえる",
+    traduction : "apprendre (quelque chose à quelqu'un), enseigner"
+  },
+{
+    mot : "逃",
+    transcription : "逃げ出す",
+    furigana : "にげだす",
+    traduction : "s'échapper, s'évader"
+  },
+{
+    mot : "湯",
+    transcription : "お湯",
+    furigana : "ゆ",
+    traduction : "eau chaude"
+  },
+{
+    mot : "追",
+    transcription : "追いかける",
+    furigana : "おいかける",
+    traduction : "poursuivre, courir après"
+  },
+{
+    mot : "次々",
+    transcription : "次々",
+    furigana : "つぎつぎ",
+    traduction : "à  la suite, un par un, l'un après l'autre"
+  },
+{
+    mot : "少",
+    transcription : "少し",
+    furigana : "すこし",
+    traduction : "un peu"
+  },
+{
+    mot : "足",
+    transcription : "足",
+    furigana : "あし",
+    traduction : "pied, jambe"
+  },
+{
+    mot : "転",
+    transcription : 転げる
+    furigana : "ころげる",
+    traduction : "se rouler, se retourner"
+  },
+{
+    mot : "下",
+    transcription : "下る",
+    furigana : "くだる",
+    traduction : "descendre"
+  },
+{
+    mot : "町",
+    transcription : "町",
+    furigana : "まち",
+    traduction : "rue, route; quartier, ville"
+  },
+{
+    mot : "帰",
+    transcription : "帰る",
+    furigana : "かえる",
+    traduction : "retourner, revenir, rentrer"
+  },
+{
+    mot : "宿屋",
+    transcription : "宿屋",
+    furigana : "やどや",
+    traduction : "hôtel, auberge"
+  },
+{
+    mot : "調",
+    transcription : "調べる",
+    furigana : "しらべる",
+    traduction : "consulter, chercher, vérifier"
+  },
+{
+    mot : "部分",
+    transcription : "部分",
+    furigana : "ぶぶん",
+    traduction : "partie, morceau"
+  },
+{
+    mot : "毛",
+    transcription : "毛",
+    furigana : "け",
+    traduction : "poil, fourrure, cheveu"
+  },
+{
+    mot : "生",
+    transcription : "生える",
+    furigana : "はえる",
+    traduction : "pousser"
+  },
+{
+    mot : "時",
+    transcription : "あの時",
+    furigana : "あのとき",
+    traduction : "cette fois-là, à ce moment-là"
+  },
+{
+    mot : "今頃",
+    transcription : "今頃",
+    furigana : "いまごろ",
+    traduction : "à cette période; seulement maintenant"
+  },
+{
+    mot : "二度",
+    transcription : "二度",
+    furigana : "にど",
+    traduction : "deux fois"
   }
+
 ];
 // Fin Demon Slayer - Opening 1
 
